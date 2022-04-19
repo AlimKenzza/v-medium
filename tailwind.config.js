2cfde678e7eb71a58566e60a23eb973cf03d4719
aimangazin <alimkenz02@gmail.com>
2022-04-19 18:08:03 +0600

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,5 +30,5 @@ module.exports = {
     variants: {
       extend: {},
     },
-    plugins: [require('@tailwindcss/forms'),require('@tailwindcss/typography')],
+    plugins: [require('@tailwindcss/forms'),require('@tailwindcss/typography'), require("daisyui")],
 };
